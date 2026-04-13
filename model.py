c7288462b4f31d9e5004395af71761c2b7fa975d
Gomathi selvi G <gomathikkn10a@gmail.com>
2026-04-13 13:07:34 +0530

    Refactor predict_doctor function to handle symptoms

--- model.py
+++ model.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-def predict_doctor(symptom):
-    symptom = symptom.lower()
-
-    if "fever" in symptom:
-        return "General Physician"
-    elif "skin" in symptom:
-        return "Dermatologist"
-    elif "heart" in symptom:
-        return "Cardiologist"
-    else:
-        return "Consult General Doctor"
-=======
 def predict_doctor(symptoms):
     symptoms = symptoms.lower()
 
@@ -24,4 +11,3 @@ def predict_doctor(symptoms):
         return "Ophthalmologist"
     else:
         return "General Doctor"
->>>>>>> 9a47424927eaf71f7a033d8751e3381dd5e98673
