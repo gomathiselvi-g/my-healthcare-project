c396171ae7b97b2981de14a81e87231d86a64389
gomathiselvi-g <gomathikkn10a@gmail.com>
2026-04-13 13:35:11 +0530

    add

--- api.py
+++ api.py
@@ -1,22 +1,9 @@
 from fastapi import FastAPI
-<<<<<<< HEAD
-=======
 from pydantic import BaseModel
->>>>>>> 9a47424927eaf71f7a033d8751e3381dd5e98673
 from model import predict_doctor
 
 app = FastAPI()
 
-<<<<<<< HEAD
-@app.get("/")
-def home():
-    return {"message": "AI Healthcare API Running"}
-
-@app.get("/predict")
-def predict(symptom: str):
-    doctor = predict_doctor(symptom)
-    return {"doctor": doctor}
-=======
 class Patient(BaseModel):
     name: str
     symptoms: str
@@ -32,4 +19,3 @@ def predict(data: Patient):
         "patient": data.name,
         "recommended_doctor": doctor
     }
->>>>>>> 9a47424927eaf71f7a033d8751e3381dd5e98673
